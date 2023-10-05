d3876f764f2be49a1759ec9442d6dd1ceff09916
kimulu <kimulumichael@gmail.com>
2023-10-05 05:18:17 -0700

    c file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char myString[] = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", myString);
+return (0);
+}
