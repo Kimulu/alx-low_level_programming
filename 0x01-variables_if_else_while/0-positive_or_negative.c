8d98a49f7fd4ae4832169e7afc428cab697602ea
kimulu <kimulumichael@gmail.com>
2023-10-06 02:38:18 -0700

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -12,15 +13,15 @@ int main(void)
 int n;
 if (n > 0)
 {
-printf("%d is positive\n", n)
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative\n", n)
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero\n", n)
+printf("%d is zero\n", n);
 }
 
 	srand(time(0));
