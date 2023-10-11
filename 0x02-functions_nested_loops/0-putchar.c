7583c072f25694776a9552e71c63d6892ced3039
kimulu <kimulumichael@gmail.com>
2023-10-11 03:18:49 -0700

    new file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* printStr - Description of the function.
+*
+* @str: Description of the first parameter.
+*/
+void printStr(const char *str)
+{
+for (int i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+}
+/**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-putchar("_putchar");
-putchar('\n');
+const char *myString = "_putchar";
+printStr(myString);
 return (0);
 }
