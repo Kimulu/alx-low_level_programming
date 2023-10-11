38398306737537bb7c8a5a835586320251906c13
kimulu <kimulumichael@gmail.com>
2023-10-11 04:45:31 -0700

    new file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet_x10(void)
 {
-char letter = 'a';
 int number = 0;
-while (number < 11)
+while (number < 10)
 {
+char letter = 'a';
 while (letter <= 'z')
 {
 putchar(letter);
