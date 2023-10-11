5d5c48bfdc727e4f4a70d4d820bda5e5f3b67437
kimulu <kimulumichael@gmail.com>
2023-10-11 10:44:28 -0700

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - a function that adds two numbers together
+ * @k: integer value
+ * @j: integer value
+ * Return: Returns the sum
+ */
+
+int add(int k, int j)
+{
+int f = (k + j);
+_putchar(f + '0');
+}
