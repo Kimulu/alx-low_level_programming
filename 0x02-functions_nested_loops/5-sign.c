2780d094da3e29db03efec5abb1b44d4f374103c
kimulu <kimulumichael@gmail.com>
2023-10-11 05:55:51 -0700

    new file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_sign - Returns 1 if the character is upper case else it returns 0.
+* Return: 1 if positive -1 if negative other wise 0.
+* @n: The character to be evaluated
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+putchar('-');
+return (-1);
+}
+else
+{
+putchar('0');
+return (0);
+}
+}
