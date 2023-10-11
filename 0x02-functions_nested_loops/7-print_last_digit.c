759e2310abd0eb4be33f7b94ddba46af4f6827a0
kimulu <kimulumichael@gmail.com>
2023-10-11 08:02:58 -0700

    new file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit - Returns the last digit of a given number.
+* Return: Return last digit.
+* @v: The number  to be evaluated
+*/
+int print_last_digit(int v)
+{
+return (v % 10);
+}
