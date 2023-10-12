c367b900f5d53a117257410a7cf18a4655d0b68c
kimulu <kimulumichael@gmail.com>
2023-10-11 23:47:51 -0700

    new file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+/*
+*_isupper - Function for checking if the character is upper
+*/
+int _isupper(int c)
+{
+if (isupper(c)) 
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
