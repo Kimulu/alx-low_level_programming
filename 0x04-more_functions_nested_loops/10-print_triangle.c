b36c5044de2d7ccff1a2dc87558c207ba5c2626c
kimulu <kimulumichael@gmail.com>
2023-10-12 05:01:12 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_triangle - Prints a triangle using "#"
+* @size: Size of triangle to be printed
+*/
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
