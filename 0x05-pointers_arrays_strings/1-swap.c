8a2cf6a14ce5956777ca5c3ec3566d43ea6714b2
kimulu <kimulumichael@gmail.com>
2023-10-16 01:18:15 -0700

    new file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*swap_int - Function that swaps values of two ints
+*@a: variable for storing the value of a
+*@b: varibale for storing thr value of b
+*/
+void swap_int(int *a, int *b)
+{
+a = &b
+b = &a
+}
