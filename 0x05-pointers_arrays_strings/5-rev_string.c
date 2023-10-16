dc151ab5df84d942ef39adcf4965627e5e9698ca
kimulu <kimulumichael@gmail.com>
2023-10-16 03:05:49 -0700

    new file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,32 +1,30 @@
-#include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include "main.h"
 /**
 *rev_string- Function for reversing a string
 *@s: variable for the string
 */
 void rev_string(char *s)
 {
-int length = strlen(s);
 char *start = s;
-char *end = s + length - 1;
+char *end = s + strlen(s) - 1;
+char temp;
 while (start < end)
 {
 if (*start != ' ' && *end != ' ')
 {
-char temp = *start;
+temp = *start;
 *start = *end;
 *end = temp;
 }
-else if (*start == ' ')
-{
-start++;
-}
-else if (*end == ' ')
+if (*start != ' ')
 {
 end--;
 }
+if (*end != ' ')
+{
 start++;
-end--;
+}
 }
 }
