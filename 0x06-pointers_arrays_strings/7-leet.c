729159eb0ed2a17e2330495b425a14e138f8f462
kimulu <kimulumichael@gmail.com>
2023-10-19 01:37:28 -0700

    new commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,19 @@ char *leet(char *str)
 char *res = str;
 while (*str)
 {
-int i;
-for (i = 0; i < 5; i++)
+for (int i = 0; i < 10; i++)
 {
-if ((*str == "aeotl"[i] || *str == "AEOTL"[i]))
+if ((*str == 'a' || *str == 'A') && (i == 4) ||
+(*str == 'e' || *str == 'E') && (i == 3) ||
+(*str == 'o' || *str == 'O') && (i == 0) ||
+(*str == 't' || *str == 'T') && (i == 7) ||
+(*str == 'l' || *str == 'L') && (i == 1))
 {
-*str = '0' + "43071"[i];
+*str = i + '0';
 break;
 }
 }
-if (i == 5)
-{
 str++;
-}
 }
 return (res);
 }
