743b1fd80a32114e1e90bb83d2ccdcdd7096f2be
kimulu <kimulumichael@gmail.com>
2023-10-19 00:27:11 -0700

    new commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-#include <ctype.h>
 /**
 *string_toupper - function for changing a string to uppercase
-*@str: the string to be evaluated
+*@str: the string to be converted
 *Return: return a string that is uppercase
 */
 char *string_toupper(char *str)
 {
+char *res = str;
 while (*str)
 {
-if (islower(*str))
+if (*str >= 'a' && *str <= 'z')
 {
-return (toupper(*str));
+*str = *str - ('a' - 'A');
 }
 str++;
 }
-return ('\0');
+return (res);
 }
