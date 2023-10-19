66d08ada36838d34260e433e28c83e781a949206
kimulu <kimulumichael@gmail.com>
2023-10-19 01:31:54 -0700

    new file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*leet - function for encoding a string
+*@str: the string to be converted
+*Return: return a string that is uppercase
+*/
+char *leet(char *str)
+{
+char *res = str;
+while (*str)
+{
+int i;
+for (i = 0; i < 5; i++)
+{
+if ((*str == "aeotl"[i] || *str == "AEOTL"[i]))
+{
+*str = '4' + i;
+break;
+}
+}
+if (i == 5)
+{
+str++;
+}
+}
+return (res);
+}
