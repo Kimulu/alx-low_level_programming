d14ecc53bba44572dcb4951ea484bf15000ff63e
kimulu <kimulumichael@gmail.com>
2023-10-19 01:44:12 -0700

    new file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,23 @@ char *leet(char *str)
 char *res = str;
 while (*str)
 {
-for (int i = 0; i < 10; i++)
+int i;
+for (i = 0; i < 5; i++)
 {
-if ((*str == 'a' || *str == 'A') && (i == 4) ||
-(*str == 'e' || *str == 'E') && (i == 3) ||
-(*str == 'o' || *str == 'O') && (i == 0) ||
-(*str == 't' || *str == 'T') && (i == 7) ||
-(*str == 'l' || *str == 'L') && (i == 1))
+if (((*str == 'a' || *str == 'A') && (i == 4)) ||
+((*str == 'e' || *str == 'E') && (i == 3)) ||
+((*str == 'o' || *str == 'O') && (i == 0)) ||
+((*str == 't' || *str == 'T') && (i == 7)) ||
+((*str == 'l' || *str == 'L') && (i == 1)))
 {
-*str = i + '0';
+*str = '0' + "43071"[i];
 break;
 }
 }
+if (i == 5)
+{
 str++;
+}
 }
 return (res);
 }
