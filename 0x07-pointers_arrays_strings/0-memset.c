24d22eaaff59391a4ef09ffdf8dcff7de3b272b4
kimulu <kimulumichael@gmail.com>
2023-10-23 05:41:01 -0700

    new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i = 0;
-for (i = 0; i < n; i++)
+while (i < n)
 {
 s[i] = b;
-}
-for (i = n; i < 100; i++)
-{
-s[i] = 0x00;
+i++;
 }
 return (s);
 }
