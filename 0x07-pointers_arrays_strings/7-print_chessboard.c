785630dfdf5f4e9eddff905d7852a54e6cf49df3
kimulu <kimulumichael@gmail.com>
2023-10-23 11:05:34 -0700

    new commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*print_chessboard - function prints a chess board
-*@a: 2d array that has the chess numbers
+*print_diagsums - function that prints the sum of the diagonals
+*@a: 2d array pointer
+*@size: the size of the array
 */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
+int sum_primary = 0;
+int sum_secondary = 0;
 int i = 0;
-while (i < 8)
+while (i < size)
 {
-int j = 0;
-while (j < 8)
-{
-_putchar(a[i][j]);
-j++;
-}
-_putchar('\n');
+sum_primary += a[i * size + i];
+sum_secondary += a[i * size + (size - 1 - i)];
 i++;
 }
+printf("%d, %d\n", sum_primary, sum_secondary);
 }
