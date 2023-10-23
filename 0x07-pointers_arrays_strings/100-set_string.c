0474be184154f82c6474dcc598bdded5ff8322d6
kimulu <kimulumichael@gmail.com>
2023-10-23 11:20:23 -0700

    new file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*set_string - function for setting string to char
+*@s: the string to be set to char
+*@to: the string to be used to set
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
