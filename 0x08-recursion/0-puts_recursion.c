1992f8aaab78972de20c0328315f4eba38e42d3f
kimulu <kimulumichael@gmail.com>
2023-10-25 01:21:24 -0700

    new commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_puts_recursion - function for printing a string
+*@s: the string
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+putchar(*s);
+_puts_recursion(s + 1);
+}
