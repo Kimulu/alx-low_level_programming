c7c4d5bc340f50af79f47d89b1e02d753f8c8cf9
kimulu <kimulumichael@gmail.com>
2023-10-27 05:12:36 -0700

    new file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
 *main - function for printing its name
+*Return: return 0
 */
-int main(void) {
-    printf("%s\n", __FILE__);
-    return 0;
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
 }
