3b242d8525c2984053c9b67d04ac65949386e5df
kimulu <kimulumichael@gmail.com>
2023-10-27 05:39:19 -0700

    new commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - function for printing number of args
+*Return: returns 0
+*@argc: argument count
+*@argv: array of strings (character pointers)
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
