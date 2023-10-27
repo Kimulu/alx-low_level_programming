a5188db006fa37299d13cd3f3d5e33addc8e9c98
kimulu <kimulumichael@gmail.com>
2023-10-27 02:57:04 -0700

    new commit

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+c_files=$(find . -maxdepth 1 -name "*.c")
+
+
+for source_file in $c_files; do
+    object_file="${source_file%.c}.o"
+    gcc -c "$source_file" -o "$object_file"
+done
+
+ar -rcs liball.a *.o
+
+rm -f *.o
