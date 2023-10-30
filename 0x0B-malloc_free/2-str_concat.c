e530827bf80297cd54ae4f8957561b1c39927d65
kimulu <kimulumichael@gmail.com>
2023-10-30 06:06:04 -0700

    new commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,48 +13,24 @@ char *temp1;
 char *temp2;
 char *result;
 char *r;
-char *s;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
 len1 = 0;
 len2 = 0;
-temp1 = s1;
-temp2 = s2;
-while (*temp1)
-{
+while (s1[len1])
 len1++;
-temp1++;
-}
-while (*temp2)
-{
+while (s2[len2])
 len2++;
-temp2++;
-}
 result = (char *)malloc(len1 + len2 + 1);
-if (result == NULL)
-{
+if (!result)
 return (NULL);
-}
 r = result;
-s = s1;
-while (*s)
-{
-*r = *s;
-r++;
-s++;
-}
-s = s2;
-while (*s)
-{
-*r = *s;
-r++;
-s++;
-}
+while (*s1)
+*r++ = *s1++;
+while (*s2)
+*r++ = *s2++;
 *r = '\0';
 return (result);
 }
