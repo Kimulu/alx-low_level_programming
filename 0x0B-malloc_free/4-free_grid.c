f17189f04366886586611df4c3a1932763c4d56e
kimulu <kimulumichael@gmail.com>
2023-10-31 02:47:19 -0700

    new commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*free_grid - function to free memory allocated by 2D array
+*Return: 0
+*@grid: the array
+*@height: height of array
+*/
+void free_grid(int **grid, int height)
+{
+if (grid == NULL)
+{
+return;
+}
+int i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+i = 0;
+while (i < height)
+{
+grid[i] = NULL;
+i++;
+}
+free(grid);
+}
