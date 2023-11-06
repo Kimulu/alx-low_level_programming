3b3a5f30ae860993a6d265ae520534f67b9c99a0
kimulu <kimulumichael@gmail.com>
2023-11-06 11:29:02 -0800

    new commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,11 +15,12 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *new_dog_ptr;
 if (name == NULL || owner == NULL)
 {
 return (NULL);
 }
-dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+new_dog_ptr = malloc(sizeof(dog_t));
 if (new_dog_ptr == NULL)
 {
 return (NULL);
