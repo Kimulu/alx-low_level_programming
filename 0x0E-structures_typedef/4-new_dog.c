a877e84cf51a343f9e09c94e0be13df4e3383a0d
kimulu <kimulumichael@gmail.com>
2023-11-06 11:45:15 -0800

    new commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,39 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * new_dog - Creates a new dog with the provided data.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: This function allocates memory for a new dog structure
- *
- * Return: If successful, a pointer to the newly created dog.
+ * new_dog - new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's dog
+ * Return: new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog_ptr;
-if (name == NULL || owner == NULL)
-{
-return (NULL);
-}
-new_dog_ptr = malloc(sizeof(dog_t));
-if (new_dog_ptr == NULL)
-{
-return (NULL);
-}
-new_dog_ptr->name = strdup(name);
-new_dog_ptr->owner = strdup(owner);
-if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
-{
-free(new_dog_ptr->name);
-free(new_dog_ptr->owner);
-free(new_dog_ptr);
-return (NULL);
-}
-new_dog_ptr->age = age;
-return (new_dog_ptr);
+
+	int i = 0, j = 0, k;
+	dog_t *dog;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->name = malloc(i * sizeof(dog->name));
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		dog->name[k] = name[k];
+	dog->age = age;
+	dog->owner = malloc(j * sizeof(dog->owner));
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		dog->owner[k] = owner[k];
+	return (dog);
 }
