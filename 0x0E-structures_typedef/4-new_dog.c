8c2c2b9d56a5ef21c641690ff5cae6a231b9d5c9
kimulu <kimulumichael@gmail.com>
2023-11-06 11:24:13 -0800

    new commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - Creates a new dog with the provided data.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: This function allocates memory for a new dog structure
+ *
+ * Return: If successful, a pointer to the newly created dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+dog_t *new_dog_ptr = malloc(sizeof(dog_t));
+if (new_dog_ptr == NULL)
+{
+return (NULL);
+}
+new_dog_ptr->name = strdup(name);
+new_dog_ptr->owner = strdup(owner);
+if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
+{
+free(new_dog_ptr->name);
+free(new_dog_ptr->owner);
+free(new_dog_ptr);
+return (NULL);
+}
+new_dog_ptr->age = age;
+return (new_dog_ptr);
+}
