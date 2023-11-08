8c61ca0617622e436ee48c9b1e2eec900c8c8f09
kimulu <kimulumichael@gmail.com>
2023-11-08 03:39:39 -0800

    new commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+/**
+* array_iterator - Execute a function on each element of an array
+* @array: The array to be processed
+* @size: The number of elements in the array
+* @action: A pointer to the function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+if (array == NULL || action == NULL)
+{
+return;
+}
+size_t i = 0;
+while (i < size)
+{
+action(array[i]);
+i++;
+}
+}
