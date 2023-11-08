97673f835c90bf9b279acabbdd1cb38a9e1c34f9
kimulu <kimulumichael@gmail.com>
2023-11-08 03:23:05 -0800

    new commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
 #include "function_pointers.h"
+#include <unistd.h>
 /**
 *print_name - function to print a name
 *@name: pointer to the name
 *@f: pointer to function
 *Return:  none
 */
-void print_name(char *name, void (*f)(char))
+void print_name(char *name, void (*f)(char *))
 {
 while (*name)
 {
-f(*name);
+f(name);
 name++;
 }
 }
