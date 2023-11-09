558637372c19896ea437d647bc3f3d6acb98ff15
kimulu <kimulumichael@gmail.com>
2023-11-09 03:31:54 -0800

    new commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,11 +9,13 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+
 	va_list args;
 
 	va_start(args, n);
 
-	unsigned int i = 0;
+	i = 0;
 
 	while (i < n)
 	{
