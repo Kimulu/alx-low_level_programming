dd03cf63aa5d7fea6008aa267e3374d67e43cb77
kimulu <kimulumichael@gmail.com>
2023-11-09 02:26:37 -0800

    new commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 int sum;
 unsigned int i;
+va_list args;
 if (n == 0)
 {
 return (0);
 }
 sum = 0;
-va_list args;
 va_start(args, n);
 i = 0;
 while (i < n)
