916978e7a30107ac3e7b601f2b394e758f404671
kimulu <kimulumichael@gmail.com>
2023-11-16 07:55:35 -0800

    new commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+*list_len -  function for returning the number of elements
+*@h: the linked list
+*Return: returns the number of elements
+*/
+size_t list_len(const list_t *h)
+{
+size_t node_count = 0;
+while (h != NULL)
+{
+h = h->next;
+node_count++;
+}
+return (node_count);
+}
