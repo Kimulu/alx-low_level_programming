65b7e1abd1b139acd5826369d8c96be329340c6a
kimulu <kimulumichael@gmail.com>
2023-11-16 07:44:21 -0800

    new commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+*print_list - function for printing list
+*@h: the list
+*Return: returns the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+size_t node_count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+node_count++;
+}
+return (node_count);
+}
