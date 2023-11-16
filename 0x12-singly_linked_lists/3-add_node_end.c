7ca0f067c273280bb7a0e6b8266c02fb2a130a41
kimulu <kimulumichael@gmail.com>
2023-11-16 08:49:09 -0800

    new commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-#include <stddef.h>
+
 /**
  * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Pointer to the head of the linked list.
  * @str: String to be duplicated and stored in the new node.
- * Return: Address of the new element, or NULL if it failed.
+ *
+ * Return: Address of the new element, or (NULL) if it failed.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *current;
-list_t *new_node;
-if (str == NULL)
-return (NULL);
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-free(new_node);
-return (NULL);
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = 0;
+	while (new_node->str[new_node->len++])
+		continue;
+
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		list_t *current = *head;
+
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+
+	return (new_node);
 }
-new_node->len = 0;
-new_node->next = NULL;
-if (*head == NULL)
-{
-*head = new_node;
-return (new_node);
-}
-current = *head;
-while (current->next != NULL)
-{
-current = current->next;
-}
-current->next = new_node;
-return (new_node);
-}
+
