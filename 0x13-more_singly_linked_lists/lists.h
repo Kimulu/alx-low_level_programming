d8bc300ce566f378764f7fe7825f5136ce7e4fc9
kimulu <kimulumichael@gmail.com>
2023-11-20 07:14:00 -0800

    new commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,19 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * 
  */
-typedef struct list_s
+typedef struct listint_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-} list_t;
+int n;
+struct listint_s *next;
+} listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
