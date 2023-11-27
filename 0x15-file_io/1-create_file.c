2f8c4f6341bcb50c8b1242c40f6a30641a9d5f07
kimulu <kimulumichael@gmail.com>
2023-11-27 04:13:11 -0800

    new commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * create_file - Creates a file and writes the given text to it.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int file_descriptor;
+ssize_t bytes_written;
+if (filename == NULL)
+return (-1);
+file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (file_descriptor == -1)
+return (-1);
+if (text_content != NULL)
+{
+bytes_written = write(file_descriptor, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(file_descriptor);
+return (-1);
+}
+}
+close(file_descriptor);
+return (1);
+}
