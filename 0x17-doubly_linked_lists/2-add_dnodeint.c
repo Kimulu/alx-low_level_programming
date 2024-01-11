3ef4dd77c6800577c34c8346b4882c8c932973ae
kimulu <kimulumichael@gmail.com>
2024-01-11 04:46:27 -0800

    new commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,11 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 dlistint_t *new_node;
 
 if (head == NULL)
-return NULL;
+return (NULL);
 
 new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
-return NULL;
+return (NULL);
 
 new_node->n = n;
 new_node->prev = NULL;
@@ -28,5 +28,5 @@ if (*head != NULL)
 (*head)->prev = new_node;
 
 *head = new_node;
-return new_node;
+return (new_node);
 }
