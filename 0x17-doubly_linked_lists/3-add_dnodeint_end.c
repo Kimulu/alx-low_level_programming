6c306ea0fe63e880e76517b14b08485919002e52
kimulu <kimulumichael@gmail.com>
2024-01-11 08:06:20 -0800

    new commit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,30 +4,37 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: pointer to the pointer to the head of the list
- * @n: value to be assigned to the new node
- * Return: address of the new element, or NULL if it failed
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the pointer to the head of the list.
+ * @n: The value to be assigned to the new node.
+ * Return: The address of the new element, or NULL if it failed.
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-dlistint_t *new_node;
-
-if (head == NULL)
-return (NULL);
-
-new_node = malloc(sizeof(dlistint_t));
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+dlistint_t *current;
 if (new_node == NULL)
+{
 return (NULL);
+}
 
 new_node->n = n;
+new_node->next = NULL;
+
+if (*head == NULL) {
 new_node->prev = NULL;
-new_node->next = *head;
-
-if (*head != NULL)
-(*head)->prev = new_node;
-
 *head = new_node;
+return (new_node);
+}
+
+current = *head;
+while (current->next != NULL) {
+current = current->next;
+}
+
+current->next = new_node;
+new_node->prev = current;
+
 return (new_node);
 }
 
