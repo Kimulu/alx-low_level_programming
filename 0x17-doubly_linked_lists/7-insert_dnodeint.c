3df2e96bf79e95155772f2ea9f3f2ec855235c5f
kimulu <kimulumichael@gmail.com>
2024-01-11 10:45:50 -0800

    new commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,26 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
+
+/**
+ * create_dnode - Creates a new doubly linked list node.
+ * @n: Value to be stored in the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
+dlistint_t *create_dnode(int n)
+{
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = NULL;
+
+return (new_node);
+}
+
 /**
  * insert_dnodeint_at_index - Inserts a new node at a given position.
  * @h: Pointer to the head of the doubly linked list.
@@ -12,52 +32,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i = 0;
+dlistint_t *new_node;
+dlistint_t *current;
+unsigned int i;
+if (h == NULL)
+return (NULL);
 
-    /* Check if h is NULL */
-    if (h == NULL)
-        return NULL;
+new_node = create_dnode(n);
+if (new_node == NULL)
+return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+if (idx == 0)
+{
+new_node->next = *h;
+if (*h != NULL)
+(*h)->prev = new_node;
+*h = new_node;
+return (*h);
+}
 
-    /* Initialize the new node */
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+current = *h;
 
-    /* If idx is 0, insert at the beginning */
-    if (idx == 0) {
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return new_node;
-    }
+for (i = 0; i < idx - 1 && current != NULL; i++)
+current = current->next;
 
-    /* Traverse the list to find the node at idx - 1 */
-    current = *h;
-    while (current != NULL && i < idx - 1) {
-        current = current->next;
-        i++;
-    }
+if (current == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    /* Check if idx is beyond the end of the list */
-    if (current == NULL) {
-        free(new_node);
-        return NULL;
-    }
+new_node->next = current->next;
+new_node->prev = current;
 
-    /* Insert the new node after the node at idx - 1 */
-    new_node->next = current->next;
-    new_node->prev = current;
-    if (current->next != NULL)
-        current->next->prev = new_node;
-    current->next = new_node;
+if (current->next != NULL)
+current->next->prev = new_node;
 
-    return new_node;
+current->next = new_node;
+return (current->next);
 }
 
