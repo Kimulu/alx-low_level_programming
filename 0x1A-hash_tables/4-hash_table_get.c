a45717547c072a23f29ea90b894a53e652a78000
kimulu <kimulumichael@gmail.com>
2024-02-01 06:56:32 -0800

    new commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -6,26 +6,26 @@
  * @ht: The hash table
  * @key: The key string
  *
- * Return: The value associated with the key, or NULL if key couldn't be found
+ * Return: The value associated with the key,NULL if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *current;
+unsigned long int index;
+hash_node_t *current;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
+index = key_index((const unsigned char *)key, ht->size);
 
-    current = ht->array[index];
+current = ht->array[index];
 
-    while (current != NULL)
-    {
-        if (strcmp(current->key, key) == 0)
-            return current->value;
-        current = current->next;
-    }
+while (current != NULL)
+{
+if (strcmp(current->key, key) == 0)
+return (current->value);
+current = current->next;
+}
 
-    return (NULL);
+return (NULL);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,25 +7,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *current;
-    int printed = 0;
+unsigned long int i;
+hash_node_t *current;
+int printed = 0;
 
-    if (ht == NULL)
-        return;
+if (ht == NULL)
+return;
 
-    printf("{");
-    for (i = 0; i < ht->size; ++i)
-    {
-        current = ht->array[i];
-        while (current != NULL)
-        {
-            if (printed)
-                printf(", ");
-            printf("'%s': '%s'", current->key, current->value);
-            printed = 1;
-            current = current->next;
-        }
-    }
-    printf("}\n");
+printf("{");
+for (i = 0; i < ht->size; ++i)
+{
+current = ht->array[i];
+while (current != NULL)
+{
+if (printed)
+printf(", ");
+printf("'%s': '%s'", current->key, current->value);
+printed = 1;
+current = current->next;
+}
+}
+printf("}\n");
 }
